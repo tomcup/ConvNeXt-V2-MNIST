bf837a11afb4f06909e0b19820c8015bc55cba1d
tomcup <jiligulu7755@outlook.com>
2026-06-16 12:41:19 +0800

    新增强：微小孔洞遮挡 结构化反色

--- src/data_utils/augmentations.py
+++ src/data_utils/augmentations.py
@@ -34,6 +34,35 @@ def random_invert_patches(image, num_patches_min=1, num_patches_max=3,
         image[y:y+ph, x:x+pw] = 255 - patch
     return image
 
+def grid_based_invert(image, min_lines=1, max_lines=3, line_width=2,
+                      invert_prob=0.5, **kwargs):
+    """
+    用随机横竖线划分网格，对部分区域进行反色。
+    """
+    import random
+    h, w = image.shape[:2]
+    # 随机线条数量
+    num_h = random.randint(min_lines, max_lines)
+    num_v = random.randint(min_lines, max_lines)
+    # 线条位置
+    h_lines = sorted(random.sample(range(line_width+2, h-line_width-2), num_h))
+    v_lines = sorted(random.sample(range(line_width+2, w-line_width-2), num_v))
+    # 画白线
+    for y in h_lines:
+        image[y-line_width:y+line_width, :] = 255
+    for x in v_lines:
+        image[:, x-line_width:x+line_width] = 255
+    # 网格区域反色
+    y_borders = [0] + h_lines + [h]
+    x_borders = [0] + v_lines + [w]
+    for i in range(len(y_borders)-1):
+        for j in range(len(x_borders)-1):
+            if random.random() < invert_prob:
+                y1, y2 = y_borders[i], y_borders[i+1]
+                x1, x2 = x_borders[j], x_borders[j+1]
+                image[y1:y2, x1:x2] = 255 - image[y1:y2, x1:x2]
+    return image
+
 
 # ---- 增强构建函数 ----
 def build_train_transform(cfg: dict) -> A.Compose:
@@ -77,6 +106,18 @@ def build_train_transform(cfg: dict) -> A.Compose:
                 hole_height_range=cd['hole_height_range'],
                 hole_width_range=cd['hole_width_range'],
                 p=cd.get('probability', 0.5)
+            )
+        )
+        
+    # 微小孔洞遮挡
+    if 'tiny_dropout' in aug_cfg:
+        td = aug_cfg['tiny_dropout']
+        transforms.append(
+            A.CoarseDropout(
+                num_holes_range=td['num_holes_range'],
+                hole_height_range=td['hole_height_range'],
+                hole_width_range=td['hole_width_range'],
+                p=td.get('probability', 0.5)
             )
         )
 
@@ -130,6 +171,19 @@ def build_train_transform(cfg: dict) -> A.Compose:
             A.Lambda(
                 image=partial(random_invert_patches, num_patches_min=num_min, num_patches_max=num_max, max_patch_size=max_size),
                 p=ri.get('probability', 0.3)
+            )
+        )
+        
+    if 'grid_invert' in aug_cfg:
+        gi = aug_cfg['grid_invert']
+        transforms.append(
+            A.Lambda(
+                image=partial(grid_based_invert,
+                              min_lines=gi.get('min_lines', 1),
+                              max_lines=gi.get('max_lines', 3),
+                              line_width=gi.get('line_width', 2),
+                              invert_prob=gi.get('invert_prob', 0.5)),
+                p=gi.get('probability', 0.25)
             )
         )
 
